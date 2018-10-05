7c4c2555df2621432349cfd2c788ed52e2175066
Jeunide <jeuteope@gmail.com>
2018-10-05 14:34:29 +0800

    Added CRUD FOR EVENTS with GUI

---
+++ src/main/java/com/okta/developer/jugtours/model/EventRepository.java
@@ -0,0 +1,8 @@
+package com.okta.developer.jugtours.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface EventRepository extends JpaRepository<Event, Long>{
+}
